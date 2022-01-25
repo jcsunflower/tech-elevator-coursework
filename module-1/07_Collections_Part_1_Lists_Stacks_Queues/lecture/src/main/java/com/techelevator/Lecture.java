dea2276e8df77462d574440deaf96860d686cdde
Jay Chen <jaycsun1@gmail.com>
2022-01-25 12:05:36 -0500

    Submitting week 2 day 2 exercise

--- module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,16 +1,28 @@
 package com.techelevator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Lecture {
 
 	public static void main(String[] args) {
+
+		Lecture lecture = new Lecture();
+		lecture.testAutoBoxing();
+
 		System.out.println("####################");
 		System.out.println("       LISTS");
 		System.out.println("####################");
+		List<String> names = new ArrayList<>();
+		names.add("Dave");
+		names.add("Sarah");
+		names.add("Bob");
+		System.out.println(names.get(2));
 
+		List<Integer> zipCodes = new ArrayList<>(Arrays.asList(45066, 45750, 45342));
+		System.out.println(zipCodes);
 
 		System.out.println("####################");
 		System.out.println("Lists are ordered");
@@ -26,6 +38,14 @@ public class Lecture {
 		System.out.println("Lists allow elements to be inserted in the middle");
 		System.out.println("####################");
 
+		List<String> planets = new ArrayList<>();
+		planets.add("Mercury");
+		planets.add("Venus");
+		planets.add("Earth");
+
+		for (int i = 0; i < planets.size(); i++) {
+			System.out.println(planets.get(i));
+		}
 
 		System.out.println("####################");
 		System.out.println("Lists allow elements to be removed by index");
@@ -63,5 +83,18 @@ public class Lecture {
 		System.out.println();
 
 
+	}
+
+	public void testAutoBoxing() {
+		System.out.println("In testAutoBoxing");
+
+		Integer x = 5;
+		Double PI = 3.14159;
+
+		double dPI = PI;
+
+		if (dPI == PI) {
+			System.out.println("dPI equals PI");
+		}
 	}
 }
