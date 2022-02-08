c9732a8f7264e430082262b2073b7ad2f1e68486
Jay Chen <jaycsun1@gmail.com>
2022-02-08 16:10:27 -0500

    Submitting week 4 day 2 exercise

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,69 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        //Initializing scanner object to take in user input
+        Scanner userInput = new Scanner(System.in);
 
+        //Obtaining information from user
+        System.out.println("What is the search word?");
+        String wordToBeReplaced = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String wordToReplace = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourcePath = userInput.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationPath = userInput.nextLine();
+
+        userInput.close();
+
+        try (PrintWriter fileOut = new PrintWriter(destinationPath)) {
+
+            // Creating scanner object to scan through our text file
+            Scanner fileReader = new Scanner(new File(sourcePath));
+
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+
+                if (line.contains(wordToBeReplaced)) {
+                    fileOut.println(line.replace(wordToBeReplaced, wordToReplace));
+                }
+                else {
+                    fileOut.println(line);
+                }
+            }
+            fileOut.close();
+            fileReader.close();
+        }  catch (FileNotFoundException e) {
+            System.err.println("Cannot open the file for writing");
+        }
+    }
+
+    /* This helper method allows the user to input the name of a text file
+    for the scanner to read
+     */
+    @SuppressWarnings("resource")
+    private static File getInputFileFromUser(String filePath) {
+
+        File inputFile = new File(filePath);
+        if (!inputFile.exists()) {
+            System.out.println(filePath + " does not exist.");
+            System.exit(1);
+        } else if (!inputFile.isFile()) {
+            System.out.println(filePath + " is not a file.");
+            System.exit(1);
+        }
+        return inputFile;
     }
 
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,8 +1,41 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
 	public static void main(String[] args) {
+
+		//Initializing scanner to allow for user input
+		Scanner userInput = new Scanner(System.in);
+
+		//Prompt the user to provide the destination file
+		System.out.print("Please enter the destination file: ");
+		String destinationPath = userInput.nextLine();
+
+		try (PrintWriter writer = new PrintWriter(new File(destinationPath))) {
+			for (int i = 1; i <= 300; i++) {
+				String output = "";
+				if (i % 3 == 0 && i % 5 == 0) {
+					output = "FizzBuzz";
+				}
+				else if (i % 3 == 0) {
+					output = "Fizz";
+				}
+				else if (i % 5 == 0) {
+					output = "Buzz";
+				}
+				else {
+					output = Integer.toString(i);
+				}
+				writer.println(output);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
