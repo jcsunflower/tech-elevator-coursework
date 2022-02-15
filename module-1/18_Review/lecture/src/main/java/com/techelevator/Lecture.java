9e723d3b967bf5ea11403f1641164db32ab99039
Jay Chen <jaycsun1@gmail.com>
2022-02-15 12:54:42 -0500

    Completed week 5 day 2 exercise

--- module-1/18_Review/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/18_Review/lecture/src/main/java/com/techelevator/Lecture.java
@@ -7,7 +7,18 @@ import java.util.Set;
 public class Lecture {
 
 	public static void main(String[] args) {
+		int[] nums = new int[] {1, 5, 7};
+		int sum = 0;
 
+		try {
+			for (int i = 0; i < Integer.MAX_VALUE; i++) {
+				sum += nums[i];
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("fucker");
+		}
+
+		System.out.println("sum = " + sum);
 	}
 
 }
