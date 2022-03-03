84d377335c7c9126e33e66315e9572d74d9d7340
Jay Chen <jaycsun1@gmail.com>
2022-03-03 18:01:22 -0500

    Completed week 7 day 2 exercise

--- module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,13 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +19,39 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
     }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Auction> list(@RequestParam(required = false) String title_like,
+                              @RequestParam(required = false) Double currentBid_lte) {
+        List<Auction> auctions = new ArrayList<>();
+
+        boolean haveTitleCriteria = title_like != null;
+        boolean haveBidCriteria = currentBid_lte != null;
+
+        if (!haveTitleCriteria && !haveBidCriteria) {
+            auctions = this.dao.list();
+        }
+        else if (haveTitleCriteria && haveBidCriteria) {
+            auctions = this.dao.searchByTitleAndPrice(title_like, currentBid_lte);
+        }
+        else if (haveTitleCriteria && !haveBidCriteria) {
+            auctions = this.dao.searchByTitle(title_like);
+        }
+        else if (!haveTitleCriteria && haveBidCriteria) {
+            auctions = this.dao.searchByPrice(currentBid_lte);
+        }
+        return auctions;
+    }
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id) {
+        return this.dao.get(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction create(@RequestBody Auction newAuction) {
+        return this.dao.create(newAuction);
+    }
+
 
 }
